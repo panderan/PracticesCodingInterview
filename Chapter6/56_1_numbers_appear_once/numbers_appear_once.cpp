901959fabf5b743322581f0ec5773f388486be2f
Pan Deran <panderan@163.com>
2019-09-20 17:27:52 +0800

    numbers appear once

--- Chapter6/56_1_numbers_appear_once/numbers_appear_once.cpp
+++ Chapter6/56_1_numbers_appear_once/numbers_appear_once.cpp
@@ -1,10 +1,55 @@
 #include "numbers_appear_once.h"
 #include <unistd.h>
+#include <cstdlib>
+
+int find_num_appear_once(int data[], int length, int *retval)
+{
+    int i = 0;
+    int &ret = *retval; 
+
+    ret = 0;
+    for (i=0; i<length; i++) {
+        ret = ret ^ data[i];
+    }
+
+    return 0;
+}
 
 int find_nums_appear_once(int data[], int length, int retvals[], int r_len)
 {
     if (data == NULL || length < 1 || retvals == NULL || r_len < 1) {
         return -1;
     }
+    
+    int ret = 0, i = 0, j = 0;
+    int *data1 = NULL, *data2 = NULL, d1_idx = 0, d2_idx = 0;
+
+    for (i=0; i<length; i++) {
+        ret = ret^data[i];
+    }
+    
+    for (i=0; i<32; i++) {
+        if ((0x1<<i) & ret) {
+            j = i;
+            break;
+        }
+    }
+
+    data1 = (int*) calloc(length, sizeof(int));
+    data2 = (int*) calloc(length, sizeof(int));
+    
+    for (i=0; i<length; i++) {
+        if ((0x1<<j) & data[i]) {
+            data1[d1_idx++] = data[i];
+        }
+        else {
+            data2[d2_idx++] = data[i];
+        }
+    }
+    
+    find_num_appear_once(data1, d1_idx, &retvals[0]);
+    find_num_appear_once(data2, d2_idx, &retvals[1]);
+
+    return 0;
 }
 
--- Chapter6/56_1_numbers_appear_once/utest.cpp
+++ Chapter6/56_1_numbers_appear_once/utest.cpp
@@ -16,6 +16,21 @@ void unit_test::tearDown()
 
 void unit_test::first_test()
 {
+    int data1[] = { 2, 4, 3, 6, 3, 2, 5, 5 };
+    int retvals1[2];
+    find_nums_appear_once(data1, sizeof(data1)/sizeof(int), retvals1, 2);
+    CPPUNIT_ASSERT_MESSAGE("Test1", (retvals1[0]==4&&retvals1[1]==6)||((retvals1[0]==6&&retvals1[1]==4)));
+
+    int data2[] = { 4, 6 };
+    int retvals2[2];
+    find_nums_appear_once(data2, sizeof(data2)/sizeof(int), retvals2, 2);
+    CPPUNIT_ASSERT_MESSAGE("Test2", (retvals2[0]==4&&retvals2[1]==6)||((retvals2[0]==6&&retvals2[1]==4)));
+
+    int data3[] = { 4, 6, 1, 1, 1, 1 };
+    int retvals3[2];
+    find_nums_appear_once(data3, sizeof(data3)/sizeof(int), retvals3, 2);
+    CPPUNIT_ASSERT_MESSAGE("Test3", (retvals3[0]==4&&retvals3[1]==6)||((retvals3[0]==6&&retvals3[1]==4)));
+
     return;
 }
 
